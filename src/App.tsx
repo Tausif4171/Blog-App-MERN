6ba3ea496487240bc6d97d269f3c072061f45bc7
Tausif4171 <tausifkhan4171@gmail.com>
2023-06-27 23:15:07 +0530

    heading given 🍀

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+    <h1 className="text-3xl font-bold text-center text-[#000]">
+      Blog App!
     </h1>
   );
 }
