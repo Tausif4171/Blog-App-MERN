d62f136371f367c4a4c8a8443138293b91ef4872
Tausif4171 <tausifkhan4171@gmail.com>
2023-06-29 20:49:22 +0530

    updated tags ✔

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className='flex flex-row justify-around items-center bg-[#fff] w-full h-[50px] text-[#000]'>
-            <div>
-                <h1 className='text-[22px]'>MyBlog</h1>
-            </div>
-            <div>
-                <ul className='flex flex-row gap-4 text-[16px]'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-        </div>
+        <main>
+            <header className='flex flex-row justify-around items-center bg-[#fff] w-full h-[50px] text-[#000]'>
+                <a href='' className='text-[22px]'>MyBlog</a>
+                <nav>
+                    <ul className='flex flex-row gap-4 text-[16px]'>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Blog</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </header>
+        </main>
     )
 }
 
