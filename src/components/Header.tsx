0670db89dd981287e96af0bc9ce0af502c8957a0
Tausif4171 <tausifkhan4171@gmail.com>
2023-06-29 21:38:05 +0530

    article structure created 🌐

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,6 +14,38 @@ function Header() {
                     </ul>
                 </nav>
             </header>
+            <div className='flex flex-col justify-center items-center min-h-screen'>
+                <div className='w-[30%] mb-10 bg-slate-50'>
+                    <img src='https://techcrunch.com/wp-content/uploads/2023/06/oak-park-river-forest-high-school.jpg?w=1390&crop=1' className='w-full' />
+                    <h1>High school changes every student’s password to ‘Ch@ngeme!’</h1>
+                    <p>
+                        After a cybersecurity audit mistakenly reset everyone’s password, a high school changed every student’s password to
+                        “Ch@ngeme!” giving every student the chance to hack into any other student’s account, according to emails obtained by
+                        TechCrunch.
+                    </p>
+                </div>
+
+                <div className='w-[30%] mb-10 bg-slate-50'>
+                    <img src='https://techcrunch.com/wp-content/uploads/2023/06/oak-park-river-forest-high-school.jpg?w=1390&crop=1' className='w-full' />
+                    <h1>High school changes every student’s password to ‘Ch@ngeme!’</h1>
+                    <p>
+                        After a cybersecurity audit mistakenly reset everyone’s password, a high school changed every student’s password to
+                        “Ch@ngeme!” giving every student the chance to hack into any other student’s account, according to emails obtained by
+                        TechCrunch.
+                    </p>
+                </div>
+
+                <div className='w-[30%] mb-10 bg-slate-50'>
+                    <img src='https://techcrunch.com/wp-content/uploads/2023/06/oak-park-river-forest-high-school.jpg?w=1390&crop=1' className='w-full' />
+                    <h1>High school changes every student’s password to ‘Ch@ngeme!’</h1>
+                    <p>
+                        After a cybersecurity audit mistakenly reset everyone’s password, a high school changed every student’s password to
+                        “Ch@ngeme!” giving every student the chance to hack into any other student’s account, according to emails obtained by
+                        TechCrunch.
+                    </p>
+                </div>
+            </div>
+
         </main>
     )
 }
