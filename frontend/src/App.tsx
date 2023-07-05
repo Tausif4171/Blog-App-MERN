b0d5d097eb06a2a5eb062e911d12a55a6bf674dd
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-05 19:57:58 +0530

    setup router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='bg-[#dbdada] w-full min-h-screen'>
-      <Header />
+      <Router>
+        <Route path="/" element={<Header />} />
+      </Router>
     </div>
   );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <main>
 
             <header className='flex flex-row justify-around items-center bg-[#fff] w-full h-[50px] text-[#000] sticky top-0'>
-                <a href='' className='text-[22px]'>MyBlog</a>
+               <Link to={'/'} className='text-[22px]'>MyBlog</Link>
                 <nav>
                     <ul className='flex flex-row gap-4 text-[16px]'>
                         <li>Home</li>
