4d98eaeb9312bd83aef64cfc0795419fda028dbc
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-09 12:20:35 +0530

    sticky header fixed ✅

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <div>
-            <header className='flex flex-row justify-around items-center bg-[#fff] w-full h-[50px] text-[#000] sticky top-0'>
+        <div className='sticky top-0'>
+            <header className='flex flex-row justify-around items-center bg-[#fff] w-full h-[50px] text-[#000]'>
                 <Link to={'/'} className='text-[22px]'>MyBlog</Link>
                 <nav>
                     <ul className='flex flex-row gap-4 text-[16px]'>
