3c338a65dfe33991ecb7619e2669c35e4edc7e26
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-09 12:24:29 +0530

    app layout created  😎

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Post from './components/Post';
 
 function App() {
   return (
     <div className='bg-[#dbdada] w-full min-h-screen'>
       <Router>
         <Routes>
-          <Route path="/" element={<Header />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Post />} />
+            <Route path='/about' element={<>about</>} />
+          </Route>
         </Routes>
       </Router>
     </div>
