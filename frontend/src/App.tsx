13fc8d85cb5dead55fcf6bc2c1ed6bad04a62a68
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-09 10:52:30 +0530

    router problem solved 😃

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='bg-[#dbdada] w-full min-h-screen'>
       <Router>
-        <Route path="/" element={<Header />} />
+        <Routes>
+          <Route path="/" element={<Header />} />
+        </Routes>
       </Router>
     </div>
   );
