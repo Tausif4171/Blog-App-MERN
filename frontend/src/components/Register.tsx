c2251507c2b43581f9f7f21fb0af9240841abfc7
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-11 20:08:43 +0530

    register page ui ✅

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Register() {
     return (
-        <div>Register</div>
+        <div>
+        <form action='POST' className='flex flex-col w-[25%] mx-auto my-[80px]'>
+        {/* <label className='align-center items-center'>Login</label> */}
+            <div className='flex flex-col'>
+                <input placeholder='Enter your email' className='mt-3 rounded p-2'/>
+                <input placeholder='Enter your password' className='mt-3 rounded p-2'/>
+            </div>
+            <button className='bg-[#555] rounded-[4px] mt-6 text-[#fff] p-2'>Register</button>
+        </form>
+    </div>
     )
 }
 
