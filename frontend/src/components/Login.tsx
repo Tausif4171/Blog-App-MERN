d2a17424559b60cf915999e48198915038485624
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-11 20:08:28 +0530

    login page ✅

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div>
+            <form action='POST' className='flex flex-col w-[25%] mx-auto my-[80px]'>
+            {/* <label className='align-center items-center'>Login</label> */}
+                <div className='flex flex-col'>
+                    <input placeholder='Enter your email' className='mt-3 rounded p-2'/>
+                    <input placeholder='Enter your password' className='mt-3 rounded p-2'/>
+                </div>
+                <button className='bg-[#555] rounded-[4px] mt-6 text-[#fff] p-2'>Login</button>
+            </form>
+        </div>
+    )
 }
 
 export default Login
