b6cee78c648e92480e1cf55c65a342fade2615f1
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-16 12:22:56 +0530

    install cors package for middleware 🚀

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Test')
 })
 
-app.post('/register',(req,res)=>{
-    const data = req.body
-    console.log(data)
-    
+app.post('/register',(req,res)=>{    
     res.status(200).json({
         message:'Registration successful'
     })
