7ea3e583c61a94db6e2f81bac0b008eab0ce57b0
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-20 00:41:04 +0530

    animate popup modal 😃

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import  './toast.css'
+import './toast.css'
 function Login() {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -22,15 +22,18 @@ function Login() {
                 <button className='bg-[#555] rounded-[4px] mt-6 text-[#fff] p-2'>Login</button>
             </form>
 
-             {isModalOpen && (
-        <div className="modal fixed left-1/2 top-1/4 transform -translate-x-1/2 -translate-y-1/4 bg-white-primary w-[459px] rounded-[8px] h-auto shadow-md animate-fade-in">
-          <div className="modal-content">
-            <h3 className="modal-heading">Modal Title</h3>
-            <p>Modal Content</p>
-            <button onClick={handleCloseModal}>Close Modal</button>
-          </div>
-        </div>
-      )}
+            {/* <div>
+                <button onClick={handleOpenModal}>Open Modal</button>
+                {isModalOpen && (
+                    <div className={` ${isModalOpen ? 'modal-content' : 'animate-fade-out'} modal fixed left-1/2 top-1/4 bg-[#fff] w-[459px] rounded-[8px] h-auto shadow-md`}>
+                        <div >
+                            <h3 className="modal-heading">Modal Title</h3>
+                            <p>Modal Content</p>
+                            <button onClick={handleCloseModal}>Close Modal</button>
+                        </div>
+                    </div>
+                )}
+            </div> */}
         </div>
     )
 }
