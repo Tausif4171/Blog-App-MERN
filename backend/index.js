e0be112a0012ecd35c6e2dca01a5d0cc624ba491
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-24 21:40:08 +0530

    connecting to monodb atlas and storing user registration data 🚀

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const mongoose = require('mongoose');
+const UserModel = require('./models/User');
 const app = express()
 
 app.use(cors())
@@ -9,14 +11,14 @@ app.get('/', (req, res) => {
     res.send('Test')
 })
 
-app.post('/register', (req, res) => {
+mongoose.connect('mongodb+srv://tausifkhan4173:786Srtrf7896@cluster0.0nwvlwj.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async (req, res) => {
     const { email, password } = req.body
     console.log(email, password)
 
-    res.status(200).json({
-        message: 'Registration successful',
-        requestedData: { email, password }
-    })
+    const userDoc = await UserModel.create({ email, password })
+    res.status(200).json(userDoc)
 })
 
 app.listen(4000)
