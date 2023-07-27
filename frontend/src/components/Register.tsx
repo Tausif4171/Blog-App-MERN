da8b4a0265585dc5dcb64c6ca8e1144dcab0bd3a
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-27 23:56:35 +0530

    form validation using yup library 🤘

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -5,7 +5,8 @@ import * as Yup from 'yup';
 function Register() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [errors, setErrors] = useState({});
+    const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
 
     async function register(e: any) {
         e.preventDefault()
@@ -26,37 +27,38 @@ function Register() {
         email: Yup.string().required('Name is required.'),
     });
 
-    const handleSubmit = async (e:any) => {
+    const handleSubmit = async (e: any) => {
         e.preventDefault();
 
         try {
             // Validate the form data against the schema
-            await validationSchema.validate(email, { abortEarly: false });
+            await validationSchema.validate({ email }, { abortEarly: false });
 
             // Form data is valid, perform form submission logic here...
             console.log('Form data submitted:', email);
-            register(e)
-        } catch (validationErrors:any) {
+            register(e);
+        } catch (validationErrors: any) {
             // Yup validation errors occurred, set the errors state accordingly
-            const errorsObject = {};
-            validationErrors.inner.forEach((error:any) => {
+            const errorsObject: any = {};
+            (validationErrors as Yup.ValidationError).inner.forEach((error: any) => {
                 errorsObject[error.path] = error.message;
             });
             setErrors(errorsObject);
         }
     };
 
+
     return (
         <div>
             <form className='flex flex-col w-[25%] mx-auto my-[80px]' onSubmit={handleSubmit}>
                 {/* <label className='align-center items-center'>Login</label> */}
                 <div className='flex flex-col'>
-                    <input placeholder='Enter your email' className={` mt-3 rounded p-2 border-[1px] border-solid ${errors.email?'border-[#ccc]':'border-[red]'} `}  value={email} onChange={e => setEmail(e.target.value)} />
+                    <input placeholder='Enter your email' className={` mt-3 rounded p-2 border-[1px] border-solid ${errors.email ? 'border-[#ccc]' : 'border-[red]'} `} value={email} onChange={e => setEmail(e.target.value)} />
                     {errors.email && (
-          <div className="error-message">
-           X {errors.email}
-          </div>
-        )}
+                        <div className="mt-[0.25rem] text-[red]">
+                            X {errors.email}
+                        </div>
+                    )}
                     <input placeholder='Enter your password' className='mt-3 rounded p-2 outline-none' value={password} onChange={e => setPassword(e.target.value)} />
                 </div>
                 <button className='bg-[#555] rounded-[4px] mt-6 text-[#fff] p-2 outline-none'>Register</button>
