e373dd77dffdf78c11380920848f871d7cd0a9ca
Tausif4171 <tausifkhan4171@gmail.com>
2023-07-31 15:23:24 +0530

    showing create and logout button if user login successfully ✅

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,15 +3,22 @@ import { Link } from 'react-router-dom'
 
 function Header() {
     const [email, setEmail] = useState('')
+    console.log({ email })
     const profile = async () => {
-        const response = await fetch('http://localhost:4000/profile',{
+        await fetch('http://localhost:4000/profile', {
             credentials: 'include',
-            method:'GET'
+            method: 'GET'
         })
+            .then(response => {
+                response.json().then(info => {
+                    setEmail(info.email)
+                })
+            })
+        // console.log(response)
     }
-    useEffect(()=>{
+    useEffect(() => {
         profile()
-    },[])
+    }, [])
     return (
         <div className='sticky top-0'>
             <header className='flex flex-row justify-around items-center bg-[#fff] w-full h-[50px] text-[#000]'>
@@ -23,8 +30,11 @@ function Header() {
                         <Link to={'/'}><li>Home</li></Link>
                         <Link to={'/'}><li>About</li></Link>
                         <Link to={'/'}><li>Blog</li></Link>
-                        <Link to={'/login'}><li>Login</li></Link>
-                        <Link to={'/register'}><li>Register</li></Link>
+                        {
+                            email ? <><Link to={'/login'}><li>Create</li></Link>
+                                <Link to={'/register'}><li>logout</li></Link></> : <>   <Link to={'/login'}><li>Login</li></Link>
+                                <Link to={'/register'}><li>Register</li></Link></>
+                        }
 
                     </ul>
                 </nav>
