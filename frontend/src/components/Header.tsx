7137faedf8b0f4f77e21dcc9725cfdbe5511e55f
Tausif4171 <tausifkhan4171@gmail.com>
2023-08-04 23:57:02 +0530

    header style updated ✅

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -48,23 +48,23 @@ function Header() {
     return (
         <div className='sticky top-0'>
             <header className='flex flex-row justify-around items-center bg-[#fff] w-full h-[50px] text-[#000]'>
-                <Link to={'/'} className='text-[22px]'>
+                <Link to={'/'} className='text-[22px] text-[#222222] font-bold'>
                     MyBlog
                 </Link>
                 <nav>
                     <ul className='flex flex-row gap-4 text-[16px] cursor-pointer'>
-                        <Link to={'/'}><li>Home</li></Link>
-                        <Link to={'/'}><li>About</li></Link>
-                        <Link to={'/'}><li>Blog</li></Link>
+                        <Link to={'/'}><li className='hover:text-[#222222] text-[#606060] font-medium'>Home</li></Link>
+                        <Link to={'/'}><li className='hover:text-[#222222] text-[#606060] font-medium'>About</li></Link>
+                        <Link to={'/'}><li className='hover:text-[#222222] text-[#606060] font-medium'>Blog</li></Link>
                         {loggedIn ? (
                             <>
-                                <Link to={'/login'}><li>Create</li></Link>
-                                <li onClick={logout}>Logout</li>
+                                <Link to={'/login'}><li className='hover:text-[#222222] text-[#606060] font-medium'>Create</li></Link>
+                                <li onClick={logout} className='hover:text-[#222222] text-[#606060] font-medium'>Logout</li>
                             </>
                         ) : (
                             <>
-                                <Link to={'/login'}><li>Login</li></Link>
-                                <Link to={'/register'}><li>Register</li></Link>
+                                <Link to={'/login'}><li className='hover:text-[#222222] text-[#606060] font-medium'>Login</li></Link>
+                                <Link to={'/register'}><li className='hover:text-[#222222] text-[#606060] font-medium'>Register</li></Link>
                             </>
                         )}
                     </ul>
