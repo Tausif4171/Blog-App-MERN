403db41de082942baac88f2902072a5074bb2721
Tausif4171 <tausifkhan4171@gmail.com>
2023-08-17 12:17:14 +0530

    moment library used for formatting timestamp ✅

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './toast.css'
+import moment from 'moment'
 
 interface IPost {
     title: string,
     summary: string,
     content: string,
-    cover: string
+    cover: string,
+    createdAt: string
 }
 
-function Post({ title, summary, content, cover }: IPost) {
+function Post({ title, summary, content, cover, createdAt }: IPost) {
     // const notify = () => toast.success("Wow so easy!", {
     //     position: toast.POSITION.BOTTOM_LEFT,
     //     className: 'toast-message'
@@ -22,7 +24,7 @@ function Post({ title, summary, content, cover }: IPost) {
                 <div className='p-4'>
                     <h1 className='text-[20px] font-medium mb-2'>{title}</h1>
                     <div className='pb-2'>
-                        <p className='flex gap-[8px] text-[13px]'><a className='font-semibold'>John Doe</a><time>9-07-2023 11:14</time></p>
+                        <p className='flex gap-[8px] text-[13px]'><a className='font-semibold'>John Doe</a><time>{moment(createdAt).format('D-MM-YYYY h:mmA')}</time></p>
                     </div>
                     <p className=''>
                         {summary}
