17cf032076f8db1c51f23e6049a04f78c05629ef
Tausif4171 <tausifkhan4171@gmail.com>
2023-08-27 21:52:29 +0530

    successfully fetch details of single post ✅

--- frontend/src/components/SinglePostDetail.tsx
+++ frontend/src/components/SinglePostDetail.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function SinglePostDetail() {
-  return (
-    <div>SinglePostDetail</div>
-  )
+    const [postInfo, setPostInfo] = useState(null)
+    const { id } = useParams()
+    console.log({ id },{postInfo})
+    useEffect(() => {
+        fetch(`http://localhost:4000/post/${id}`).then((response: any) => {
+            console.log({ response })
+            response.json().then((postInfo: any) => {
+                setPostInfo(postInfo)
+            })
+        })
+    }, [])
+    return (
+        <div>SinglePostDetail</div>
+    )
 }
 
 export default SinglePostDetail
