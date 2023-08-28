e908a9dd77222c56b4ee5f9b7c8f1265e2fe8304
Tausif4171 <tausifkhan4171@gmail.com>
2023-08-28 23:22:46 +0530

    update post page ✅

--- frontend/src/components/SinglePostDetail.tsx
+++ frontend/src/components/SinglePostDetail.tsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function SinglePostDetail() {
-    const [postInfo, setPostInfo] = useState(null)
+    const [postInfo, setPostInfo] = useState<any>(null)
     const { id } = useParams()
-    console.log({ id },{postInfo})
+    console.log({ id }, { postInfo })
     useEffect(() => {
         fetch(`http://localhost:4000/post/${id}`).then((response: any) => {
             console.log({ response })
@@ -14,7 +14,11 @@ function SinglePostDetail() {
         })
     }, [])
     return (
-        <div>SinglePostDetail</div>
+        <div className='flex justify-center items-center mt-[100px]'>
+            <h1 className=' flex justify-center items-center text-[22px]'>
+                {postInfo?.title}
+            </h1>
+        </div>
     )
 }
 
