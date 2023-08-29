a185916386d7d417c511a51b2d6dee4dff15d26c
Tausif4171 <tausifkhan4171@gmail.com>
2023-08-29 21:52:06 +0530

    post detail page 🦾

--- frontend/src/components/SinglePostDetail.tsx
+++ frontend/src/components/SinglePostDetail.tsx
@@ -13,11 +13,20 @@ function SinglePostDetail() {
             })
         })
     }, [])
+    function removeHtmlTags(str: any) {
+        return str.replace(/<[^>]+>/g, "");
+    }
+
+    const noHtmlTags = removeHtmlTags(postInfo?.content);
     return (
-        <div className='flex justify-center items-center mt-[100px]'>
+        <div className='flex flex-col justify-center items-center mt-[100px]'>
+            <img src={'http://localhost:4000/' + postInfo?.cover.replace(/\\/g, '/')} />
             <h1 className=' flex justify-center items-center text-[22px]'>
                 {postInfo?.title}
             </h1>
+            <p>{postInfo?.author.email}</p>
+            <p>{noHtmlTags}</p>
+            <p>{postInfo?.summary}</p>
         </div>
     )
 }
