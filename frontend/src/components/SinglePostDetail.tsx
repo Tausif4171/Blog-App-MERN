b9b8b0ba66acad9e65aea531208f6744d788aa00
Tausif4171 <tausifkhan4171@gmail.com>
2023-08-30 11:03:34 +0530

    minor ui update ⛏️

--- frontend/src/components/SinglePostDetail.tsx
+++ frontend/src/components/SinglePostDetail.tsx
@@ -13,10 +13,11 @@ function SinglePostDetail() {
             })
         })
     }, [])
-    
+
     return (
         <div className='flex flex-col justify-center items-center mt-[100px]'>
-            <img src={'http://localhost:4000/' + postInfo?.cover.replace(/\\/g, '/')} />
+            {postInfo?.cover && <img src={'http://localhost:4000/' + postInfo.cover.replace(/\\/g, '/')} />}
+
             <h1 className=' flex justify-center items-center text-[22px]'>
                 {postInfo?.title}
             </h1>
