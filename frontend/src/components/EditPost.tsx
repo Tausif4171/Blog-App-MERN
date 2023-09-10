f1a812b9248847b1438ff829ace6eb745a78f511
Tausif4171 <tausifkhan4171@gmail.com>
2023-09-10 19:04:46 +0530

    code of display file name

--- frontend/src/components/EditPost.tsx
+++ frontend/src/components/EditPost.tsx
@@ -6,10 +6,20 @@ function EditPost() {
     const [title, setTitle] = useState<any>('')
     const [summary, setSummary] = useState('')
     const [content, setContent] = useState('')
-    const [files, setFiles] = useState<FileList | null>(null);
+    const [files, setFiles] = useState<any>(null);
     console.log({ title }, { summary }, { content }, { files })
     const navigate = useNavigate()
     const { id } = useParams()
+
+    let test = ''
+    if (files) {
+        const parts = files.split("\\");
+        const filename = parts[parts.length - 1];
+        test = filename
+        console.log(filename); // Output: 3b94226b9a0c2075c4e32d6b26a027f4.png
+    } else {
+        console.log("No file selected");
+    }
 
     const updatePost = async (e: any) => {
         e.preventDefault();
@@ -42,7 +52,7 @@ function EditPost() {
             console.log({ postDetail })
             setTitle(postDetail.title)
             setSummary(postDetail.summary)
-            setFiles(postDetail.cover)
+            setFiles(`http://localhost:4000/${postDetail.cover}`);
             setContent(postDetail.content)
         }
         ))
@@ -57,7 +67,18 @@ function EditPost() {
             <form className='flex flex-col gap-y-2 w-[30%]' onSubmit={updatePost}>
                 <input type='text' placeholder='please enter title' className='flex justify-items-center p-2 outline-none' value={title} onChange={e => setTitle(e.target.value)} />
                 <input type='text' placeholder='please enter summary' className='flex justify-items-center p-2 outline-none' value={summary} onChange={e => setSummary(e.target.value)} />
-                <input type="file" onChange={e => setFiles(e.target.files)} />
+                <div>
+                    {files && (
+                        <img src={files} alt="Post Cover" style={{ maxWidth: '100%' }} />
+                    )}
+                </div>
+                <input
+                    type="file"
+                    // value={test}
+                    onChange={(e) => setFiles(e.target.files)}
+                />
+                {/* {test} */}
+
                 {/* <textarea placeholder='enter message' className='flex justify-items-center p-2' /> */}
                 <ReactQuill theme="snow" className='bg-[#fff]' value={content} onChange={setContent} />
                 <button className='w-[100%] bg-[#555] text-[#fff] p-2 mt-4 rounded-md'>Update</button>
